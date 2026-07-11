02d54da16030eff3936bf7488db89022b32251aa
Chaity <chaitymetrouni@gmail.com>
2026-07-11 23:29:24 +0600

    Lesson 1 exercise 1

---
+++ Lesson 1 practice problem.js/exercise1.js
@@ -0,0 +1,5 @@
+//Print your name, your department, and your university on three separate lines.
+
+console.log("Chaity Upadhyay");
+console.log("Computer Science and Engineering");
+console.log("Metropolitan University");
